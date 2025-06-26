a61c3b7fdae65d912044c4c4812c7d193020a4d1
jagallob <jagallob@eafit.edu.co>
2025-06-26 18:09:20 -0500

    Integración avanzada de NarrativeGenerator en UI y flujo automático

---
+++ report-builder-client/src/pages/NarrativeGeneratorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NarrativeGenerator from "../components/AI/NarrativeGenerator";
+
+const NarrativeGeneratorPage = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-white p-6">
+      <NarrativeGenerator />
+    </div>
+  );
+};
+
+export default NarrativeGeneratorPage;
